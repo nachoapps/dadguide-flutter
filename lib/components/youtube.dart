c9a760606aad4f8b86d15101b44af77152fa5bf6
tactical_retreat <tactical0retreat@gmail.com>
2019-09-10 23:10:12 -0400

    Redo youtube launching
    This should close #17

--- lib/components/youtube.dart
+++ lib/components/youtube.dart
@@ -2,30 +2,22 @@ import 'dart:io';
 
 import 'package:url_launcher/url_launcher.dart';
 
-// TODO: try other implementations on iOS like this one:
-// https://github.com/flutter/plugins/blob/master/packages/url_launcher/example/lib/main.dart
-
 /// Try to launch YT natively, falling back to a browser.
-///
-/// This seems to work on Android but not on iOS. Needs some experimentation.
 Future<void> launchYouTubeSearch(String query) async {
-  var url = 'https://www.youtube.com/results?search_query=$query';
-  var youtubePrefixUrl = 'youtube:$url';
+  var androidUrl = Uri.encodeFull('https://www.youtube.com/results?search_query=$query');
+  var iosUrl = Uri.encodeFull('youtube://www.youtube.com/results?search_query=$query');
   if (Platform.isIOS) {
-    if (await canLaunch(youtubePrefixUrl)) {
-      await launch(youtubePrefixUrl, forceSafariVC: false);
-    } else {
-      if (await canLaunch(url)) {
-        await launch(url);
-      } else {
-        throw 'Could not launch $url';
-      }
+    var launched = false;
+    launched = await launch(iosUrl, forceSafariVC: false, universalLinksOnly: true);
+    if (!launched) {
+      launched = await launch(iosUrl, forceSafariVC: false);
+    }
+    if (!launched) {
+      throw 'Could not launch $iosUrl';
     }
   } else {
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
+    if (!await launch(androidUrl)) {
+      throw 'Could not launch $androidUrl';
     }
   }
 }
