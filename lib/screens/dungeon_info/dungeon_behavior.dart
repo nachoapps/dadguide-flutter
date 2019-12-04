52a9118b12bcf5f94bb5987d8b102402afb25623
nachoapps <nachoappsinc@gmail.com>
2019-12-03 22:37:08 -0500

    More cleanups for monster behavior.
    Stick borders around each group, only identify the top level group
    type, format conditions nicely.

--- lib/screens/dungeon_info/dungeon_behavior.dart
+++ lib/screens/dungeon_info/dungeon_behavior.dart
@@ -13,6 +13,81 @@ class EnemySkillLibrary {
   EnemySkillLibrary(this.data);
 }
 
+String convertGroup(BehaviorGroup_GroupType groupType, Condition cond) {
+  switch (groupType) {
+    case BehaviorGroup_GroupType.DEATH:
+      return 'On Death';
+    case BehaviorGroup_GroupType.DISPEL_PLAYER:
+      return 'When player effect active';
+    case BehaviorGroup_GroupType.MONSTER_STATUS:
+      return 'When monster delayed/poisoned';
+    case BehaviorGroup_GroupType.PASSIVE:
+      return 'Base abilities';
+    case BehaviorGroup_GroupType.PREEMPT:
+      return 'Preemptive';
+    case BehaviorGroup_GroupType.REMAINING:
+      return 'When ${cond.triggerEnemiesRemaining} enemies remain';
+    case BehaviorGroup_GroupType.STANDARD:
+      return 'Standard';
+  }
+  return 'Unknown';
+}
+
+String formatCondition(Condition cond) {
+  var parts = <String>[];
+
+  if (![0, 100].contains(cond.useChance)) {
+    parts.add('${cond.useChance}% chance');
+  }
+
+  if (cond.hasRepeatsEvery()) {
+    if (cond.hasTriggerTurn()) {
+      parts.add('Execute repeatedly, turn ${cond.triggerTurn} of ${cond.repeatsEvery}');
+    } else {
+      parts.add('Repeats every ${cond.repeatsEvery} turns');
+    }
+  } else if (cond.hasTriggerTurnEnd()) {
+    parts.add('Turns ${cond.triggerTurn}-${cond.triggerTurnEnd}');
+  } else if (cond.hasTriggerTurn()) {
+    parts.add('Turn ${cond.triggerTurn}');
+  }
+
+  if (cond.globalOneTime) {
+    parts.add('One time only');
+  }
+
+  if (cond.hasTriggerEnemiesRemaining()) {
+    parts.add('When ${cond.triggerEnemiesRemaining} enemies remain');
+  }
+
+  if (cond.ifDefeated) {
+    parts.add('When defeated');
+  }
+
+  if (cond.ifAttributesAvailable) {
+    parts.add('When required attributes on board');
+  }
+
+  if (cond.triggerMonsters.isNotEmpty) {
+    var monsterStr = cond.triggerMonsters.map((m) => m.toString()).join(', ');
+    parts.add('When [$monsterStr] on team');
+  }
+
+  if (cond.hasTriggerCombos()) {
+    parts.add('When ${cond.triggerCombos} combos last turn');
+  }
+
+  if (cond.ifNothingMatched) {
+    parts.add('If no other skills matched');
+  }
+
+  if (![100, 0].contains(cond.hpThreshold)) {
+    parts.add('HP <= ${cond.hpThreshold}');
+  }
+
+  return parts.join(', ');
+}
+
 /// Top-level container for monster behavior. Displays a red warning sign if monster data has not
 /// been reviewed yet, then a list of BehaviorGroups.
 ///
@@ -26,47 +101,54 @@ class EncounterBehavior extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        if (!approved)
-          Container(
-            padding: EdgeInsets.all(4),
-            decoration: ShapeDecoration(
-              color: Colors.red,
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          if (!approved)
+            Container(
+              padding: EdgeInsets.all(4),
+              decoration: ShapeDecoration(
+                color: Colors.red,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+              ),
+              child: Text(
+                  'Monster data has not been reviewed and approved yet. Rely on this at your own risk.'),
             ),
-            child: Text(
-                'Monster data has not been reviewed and approved yet. Rely on this at your own risk.'),
-          ),
-        for (var group in groups) BehaviorGroupWidget(group),
-      ],
+          for (var group in groups) BehaviorGroupWidget(true, group),
+        ],
+      ),
     );
   }
 }
 
 /// A group of behavior, containing a list of child groups or individual behaviors.
 class BehaviorGroupWidget extends StatelessWidget {
+  final bool showType;
   final BehaviorGroup group;
 
-  BehaviorGroupWidget(this.group);
+  BehaviorGroupWidget(this.showType, this.group);
 
   @override
   Widget build(BuildContext context) {
-    var groupType = group.groupType;
-    var groupName = '';
-    if (groupType != BehaviorGroup_GroupType.UNSPECIFIED) {
-      groupName = groupType.name.toLowerCase();
-    }
-
-    return Column(
+    var contents = Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        if (groupName.isNotEmpty) Text('Group Type: $groupName'),
         for (var child in group.children)
-          Padding(padding: EdgeInsets.only(left: 4), child: BehaviorItemWidget(child)),
+          Padding(padding: EdgeInsets.only(left: 4, right: 4), child: BehaviorItemWidget(child)),
       ],
     );
+
+    var conditionText = formatCondition(group.condition);
+
+    if (showType) {
+      return TextBorder(text: convertGroup(group.groupType, group.condition), child: contents);
+    } else if (conditionText.isNotEmpty) {
+      return TextBorder(text: conditionText, child: contents);
+    } else {
+      return contents;
+    }
   }
 }
 
@@ -79,7 +161,7 @@ class BehaviorItemWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     if (child.hasGroup()) {
-      return BehaviorGroupWidget(child.group);
+      return BehaviorGroupWidget(false, child.group);
     } else {
       return BehaviorWidget(child.behavior);
     }
@@ -97,22 +179,55 @@ class BehaviorWidget extends StatelessWidget {
     var library = Provider.of<EnemySkillLibrary>(context);
     var skill = library.data[behavior.enemySkillId];
 
-    var condition = behavior.condition;
-    var conditionParts = [];
-    if (condition.hpThreshold != 100) {
-      conditionParts.add('HP <= ${condition.hpThreshold}');
+    var descText = skill.descNa;
+    var conditionText = formatCondition(behavior.condition);
+    if (conditionText.isNotEmpty) {
+      descText = '$descText ($conditionText)';
     }
-    if (condition.useChance != 100) {
-      conditionParts.add('Use chance ${condition.useChance}%');
-    }
-    var conditionStr = conditionParts.join(', ');
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        if (conditionStr.isNotEmpty) Text(conditionStr),
         Text(skill.nameNa, style: subtitle(context)),
-        Text(skill.descNa, style: secondary(context)),
+        Text(descText, style: secondary(context)),
+      ],
+    );
+  }
+}
+
+class TextBorder extends StatelessWidget {
+  final String text;
+  final Widget child;
+
+  const TextBorder({@required this.text, @required this.child, Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          width: double.infinity,
+          child: child,
+          // This margin forces the border down so the text can overlay it.
+          margin: EdgeInsets.only(top: 10),
+          // Force the contents down so the title doesn't overlap it. Pad the bottom by the same
+          // amount to make it symmetric.
+          padding: EdgeInsets.only(top: 10, bottom: 10),
+          decoration: BoxDecoration(
+            border: Border.all(color: Theme.of(context).primaryColor.withOpacity(.4), width: 1),
+            borderRadius: BorderRadius.circular(4),
+          ),
+        ),
+        Positioned(
+            left: 20,
+            top: 0,
+            child: Container(
+              // This padding enforces a slight gap left and right of the text
+              padding: EdgeInsets.only(left: 10, right: 10),
+              // Make the text container hide the border.
+              color: Theme.of(context).scaffoldBackgroundColor,
+              child: Text(text),
+            )),
       ],
     );
   }
